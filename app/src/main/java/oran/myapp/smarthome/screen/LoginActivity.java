7633454b23acef7fe024082ffbc8b7a8b85a4b76
TidelxD <luffygear37@gmail.com>
2022-06-08 23:41:41 +0100

    - init commit

---
+++ app/src/main/java/oran/myapp/smarthome/screen/LoginActivity.java
@@ -0,0 +1,16 @@
+package oran.myapp.smarthome.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import oran.myapp.smarthome.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
